1f802403bde2e079afd9c7048cbaecfda40c5c8b
prinzprogrammer <princeprogrammer73@gmail.com>
2023-11-03 23:58:11 +0800

    Added public methods fight and show

--- src/Hero.java
+++ src/Hero.java
@@ -21,4 +21,35 @@ public class Hero {
 		this.speed = (int)(Math.random() * 100 + 1);
 		this.health = (int)(Math.random() * 100 + 1);
 	}
+	
+	private boolean hitAttempt() {
+		int randomNumber = (int)(Math.random() * 5 + 1);
+		if (randomNumber == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private int hitDamage() {
+		int damage = (int)(Math.random() * 6 + 1) * this.strength;
+		return damage;
+	}
+	
+	// PUBLIC METHODS *******************************************
+	public boolean fight() {
+		System.out.println("The " + this.name + " is fighting!");
+		// call hitAttempt()
+		// if hitAttempt() returns true
+		// call hitDamage()
+		return true;
+	}
+	
+	public void show() {
+		System.out.println("--------------------------------");
+		System.out.println("strength: " + this.strength);
+		System.out.println("speed: " + this.speed);
+		System.out.println("health: " + this.health);
+		System.out.println("--------------------------------");
+	}
 }
