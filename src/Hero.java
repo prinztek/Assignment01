d05847b0ade60afe83946ab15f390b1eec80503d
prinzprogrammer <princeprogrammer73@gmail.com>
2023-11-03 23:31:30 +0800

    Created Hero Class with properties and constructor method

---
+++ src/Hero.java
@@ -0,0 +1,16 @@
+
+public class Hero {
+	
+	// Private Properties ********************************************
+	private int strength;
+	private int speed;
+	private int health;
+	
+	// Private Properties ********************************************
+	public String name;
+	
+	// CONSTRUCTOR *******************************************
+	public Hero(String name) {
+		this.name = name;
+	}
+}
